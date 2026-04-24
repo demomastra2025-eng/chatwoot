konversio-org/konversio

feat: support bulk select and delete for documents (#13907)

app/javascript/dashboard/store/captain/document.js
@@ -4,4 +4,12 @@ import { createStore } from '../storeFactory';
 export default createStore({
   name: 'CaptainDocument',
   API: CaptainDocumentAPI,
+  actions: mutations => ({
+    removeBulkRecords({ commit, getters }, ids) {
+      const records = getters.getRecords.filter(
+        record => !ids.includes(record.id)
+      );
+      commit(mutations.SET, records);
+    },
+  }),
 });

demomastra2025-eng/chatwoot

(cherry picked from commit 4517c5022766b477d85059de44365abf1644462d)

app/javascript/dashboard/store/captain/document.js
@@ -44,5 +44,11 @@ export default createStore({
         commit(mutationTypes.SET_UI_FLAG, { updatingItem: false });
       }
     },
+    removeBulkRecords({ commit, getters }, ids) {
+      const records = getters.getRecords.filter(
+        record => !ids.includes(record.id)
+      );
+      commit(mutationTypes.SET, records);
+    },
   }),
 });