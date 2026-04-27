d2f05cee3b585d1e470d30ae430d36bac92c05ac
demomastra2025-eng <demomastra2025-eng@users.noreply.github.com>
2026-04-27 21:53:43 +0200

    chore: sync schema version for fonoster migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_26_065856) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_27_090000) do
   create_schema "agent_transport"
   create_schema "evolution_api"
   create_schema "mastra_agent"
